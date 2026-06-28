b4d55fea8585795054446e6048af6ab12773007e
suryansh singh <singhsuryansh22621@gmail.com>
2026-06-28 16:51:55 +0530

    commit

---
+++ day 21/LowerToUpper.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class LowerToUpper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter lowercase string: ");
+        String str = sc.nextLine();
+
+        String upper = str.toUpperCase();
+
+        System.out.println("Uppercase = " + upper);
+    }
+}
